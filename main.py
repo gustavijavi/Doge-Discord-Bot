87f1902a1f1da26e177df7b840dc087b34e42512
gustavijavi <javier.collroman@gmail.com>
2026-03-14 14:12:10 -0400

    added role assigning commands and a ping command for testing

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='d!', intents=intents)
 
 # set up your own user id in the env file in case you want the bot to @ you specifically at certain points
-my_user_id = os.getenv('USER_ID')
+my_user_id = int(os.getenv('USER_ID'))
 
 @bot.event
 async def on_ready():
@@ -33,16 +33,42 @@ async def on_message(message):
 
     message_to_me = f"I'll... I'll spread the word\nWE PLAYIN <@{my_user_id}>!!!!!!"
     
-    if "league" in message.content.lower() and message.author.id != my_user_id:
+    if "play" in message.content.lower() and "league" in message.content.lower() and message.author.id != my_user_id:
         await message.channel.send(f"DID SOMEBODY SAY LEAGUE???")
-        await message.channel.sned(message_to_me)
+        await message.channel.send(message_to_me)
 
-    if "roblox" in message.content.lower() and message.author.id != my_user_id:
+    if "play" in message.content.lower() and "roblox" in message.content.lower() and message.author.id != my_user_id:
         await message.channel.send(f"DID SOMEBODY SAY BOBLOX???")
         await message.channel.send(message_to_me)
 
     await bot.process_commands(message)
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"pong {ctx.author.mention}")
 
+@bot.command()
+async def assign(ctx):
+    # can be used to assign roles later on
+    # role = discord.utils.get(ctx.guild.roles, name="whatevarole")
+    # if role:
+    #   await ctx.author.add_roles(role)
+    #   await ctx.send(f"{ctx.author.mention} is now assigned to whatevarole")
+    # else:
+    #   await ctx.send("Role doesn't exist")
+    
+    return
+
+@bot.command()
+async def unassign(ctx):
+    # can be used to unassign roles later on
+    # role = discord.utils.get(ctx.guild.roles, name="whatevarole")
+    # if role:
+    #   await ctx.author.remove_roles(role)
+    #   await ctx.send(f"Removed whatevarole from {ctx.author.mention}")
+    # else:
+    #   await ctx.send("Role doesn't exist")
+    
+    return
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
