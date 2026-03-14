dd4299da94e5afeff8d434d1c7cb067bf63cac7e
gustavijavi <javier.collroman@gmail.com>
2026-03-14 13:37:51 -0400

    bot messaging features for specific events

--- main.py
+++ main.py
@@ -14,8 +14,35 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='d!', intents=intents)
 
+# set up your own user id in the env file in case you want the bot to @ you specifically at certain points
+my_user_id = os.getenv('USER_ID')
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name}, is ready to chud it out")
+
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server chud, {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    message_to_me = f"I'll... I'll spread the word\nWE PLAYIN <@{my_user_id}>!!!!!!"
+    
+    if "league" in message.content.lower() and message.author.id != my_user_id:
+        await message.channel.send(f"DID SOMEBODY SAY LEAGUE???")
+        await message.channel.sned(message_to_me)
+
+    if "roblox" in message.content.lower() and message.author.id != my_user_id:
+        await message.channel.send(f"DID SOMEBODY SAY BOBLOX???")
+        await message.channel.send(message_to_me)
+
+    await bot.process_commands(message)
+
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
