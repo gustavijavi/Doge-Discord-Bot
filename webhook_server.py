94db7f19c8afb7dabfdcafd08a458593f94152da
gustavijavi <javier.collroman@gmail.com>
2026-03-17 15:09:42 -0400

    added comments for everything important in file

--- webhook_server.py
+++ webhook_server.py
@@ -5,24 +5,33 @@ import os
 import subprocess
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+# loads .env file
 load_dotenv()
 
 # Key for GitHub to use for sign requests
 # Stored in the .env file
 WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET', '')
 
+# class inherited from BaseHTTPRequestHandler from the import
 class WebhookHandler(BaseHTTPRequestHandler):
+
+	# function for whenever post is sent to the raspberry pi
 	def do_POST(self):
-		# Read the incoming request body
+
+		# read the incoming request body
 		content_length = int(self.headers.get("Content-Length", 0))
 		body = self.rfile.read(content_length)
 
-		# Verify the request is actually from GitHub using the secret
+		# grabs GitHub's authentication to send post to webhook server
 		signature = self.headers.get("X-Hub-Signature-256", "")
+
+		# sha-256 is the hashing used for getting the expected output from the secret and body being hashed together
 		expected = "sha256=" + hmac.new(WEBHOOK_SECRET.encode(), body, hashlib.sha256).hexdigest()
 
+		# compares the signature sent by GitHub to the one expected to be received
+		# utilizes 'hmac.compare_digest' instead of '==' because of security vulnerabilities
 		if not hmac.compare_digest(signature, expected):
-			# Secret doesn't match so ignore the request
+			# secret doesn't match so ignore the request with an error response code and message sent back
 			self.send_response(403)
 			self.end_headers()
 			self.wfile.write(b"Forbidden")
@@ -31,19 +40,25 @@ class WebhookHandler(BaseHTTPRequestHandler):
 
 		# Secret matches so pull latest code and restart bot
 		print("Valid webhook received, updating...")
+
+		# pulls the updated repository from GitHub to update bot
 		subprocess.run(["git", "pull"], cwd="/home/pi/Doge-Discord-Bot")
+
+		# restarts the bot using the 'restart_bot.sh' file
 		subprocess.run(["bash", "/home/pi/Doge-Discord-Bot/restart_bot.sh"])
 
+		# sends response code and message basically saying everything went well
 		self.send_response(200)
 		self.end_headers()
 		self.wfile.write(b"Updated")
 
 	def do_GET(self):
-		# Ignore any non-POST requests
+		# ignore any non-POST requests
+		# sends an error 404 response code
 		self.send_response(404)
 		self.end_headers()
 
-# Start the server on port 9000
+# start the server on port 9000
 server = HTTPServer(("0.0.0.0", 9000), WebhookHandler)
 print("Webhook server running on port 9000...")
 server.serve_forever()
