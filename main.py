b358abcf451e56c58cb73d29c4ec682d0a6fbe75
gustavijavi <javier.collroman@gmail.com>
2026-03-17 23:42:41 -0400

    response.json working maybe

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def getLeagueStats(ctx, *, riotName):
     
     responseCode = response.status_code
 
-    responseData = response.headers
+    responseData = response.json
 
     if responseCode != 200:
         await channel.send("Invalid Riot name twin")
